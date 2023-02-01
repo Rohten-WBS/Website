fdb7e43d0981b0787ca744580745a54fa90ab75e
Rohten (WBS) <109280187+Rohten-WBS@users.noreply.github.com>
2023-02-01 08:51:03 +0100

    Update assets_loader.js

--- assets/js/assets_loader.js
+++ assets/js/assets_loader.js
@@ -1,8 +1,4 @@
-var scripts = document.getElementById('loader');
-var path = scripts.src.split('?')[0];
-var mydir = path.split('/').slice(0, -2).join('/')+'/';
-console.log(mydir);
 $(document).ready(function () {
-  $("header").load(mydir + "header.html");
-  $("footer").load(mydir + "footer.html");
+  $("header").load("/Website/header.html");
+  $("footer").load("/Website/footer.html");
 });
