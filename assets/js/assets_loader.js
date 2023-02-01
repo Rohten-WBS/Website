5f810ea810199a319a2bff1de95fe8712c5f7269
Rohten (WBS) <109280187+Rohten-WBS@users.noreply.github.com>
2023-02-01 13:39:17 +0100

    Update assets_loader.js

--- assets/js/assets_loader.js
+++ assets/js/assets_loader.js
@@ -1,4 +1,12 @@
-$(document).ready(function () {
-  $("header").load("/Website/assets/header.html");
-  $("footer").load("/Website/assets/footer.html");
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("/Website/assets/header.html")
+    .then(response => response.text())
+    .then(header => {
+      document.querySelector("header").innerHTML = header;
+    });
+  fetch("/Website/assets/footer.html")
+    .then(response => response.text())
+    .then(footer => {
+      document.querySelector("footer").innerHTML = footer;
+    });
 });
