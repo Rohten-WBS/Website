681abb78ac80ca735ab392eac53edb401265f70a
Rohten (WBS) <109280187+Rohten-WBS@users.noreply.github.com>
2023-02-01 18:39:45 +0100

    Update assets_loader.js

--- assets/js/assets_loader.js
+++ assets/js/assets_loader.js
@@ -1,12 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetch("/Website/assets/header.html")
-    .then(response => response.text())
-    .then(header => {
+    .then((response) => response.text())
+    .then((header) => {
       document.querySelector("header").innerHTML = header;
     });
   fetch("/Website/assets/footer.html")
-    .then(response => response.text())
-    .then(footer => {
+    .then((response) => response.text())
+    .then((footer) => {
       document.querySelector("footer").innerHTML = footer;
     });
 });
+
+function AboutUs() {
+  document.addEventListener("click", function (event) {
+    var dropdown = document.querySelector("#about-us-dropdown");
+    var aboutUs = document.querySelector("#about-us");
+    if (event.target === aboutUs) {
+      dropdown.style.display = dropdown.style.display === "none" ? "" : "none";
+    } else if (!event.target.closest("#about-us-dropdown")) {
+      dropdown.style.display = "none";
+    }
+  });
+}
