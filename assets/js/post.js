3ebf23795a2e06ebf053a99d221dd8d532b0ebf9
Rohten (WBS) <109280187+Rohten-WBS@users.noreply.github.com>
2023-02-02 09:35:59 +0100

    Update post.js

--- assets/js/post.js
+++ assets/js/post.js
@@ -34,8 +34,7 @@ async function loadPostList() {
   const data = await $.ajax({
     url: "/Website/blog/posts/posts.json",
   });
-  console.log(data);
-  for (let i = data.posts.length; i >= 0; i--) {
+  for (let i = data.posts.length -1; i >= 0; i--) {
     const postId = data.posts[i].id;
     const postUrl = "/Website/blog/posts/" + postId + ".html";
     const postData = await $.get(postUrl);
