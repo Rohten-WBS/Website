d5e40dda50ecd7c788d4f0ea37b7909b21c834fa
Rohten (WBS) <109280187+Rohten-WBS@users.noreply.github.com>
2023-02-02 09:26:05 +0100

    Revert to jQuery

--- assets/js/assets_loader.js
+++ assets/js/assets_loader.js
@@ -1,19 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/Website/assets/header.html", true);
-  xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-  document.querySelector("header").innerHTML = xhr.responseText;
-  }
-  };
-  xhr.send();
-  
-  var xhr2 = new XMLHttpRequest();
-  xhr2.open("GET", "/Website/assets/footer.html", true);
-  xhr2.onreadystatechange = function () {
-  if (xhr2.readyState === 4 && xhr2.status === 200) {
-  document.querySelector("footer").innerHTML = xhr2.responseText;
-  }
-  };
-  xhr2.send();
-  });
+$(document).ready(function () {
+  $("header").load("/Website/assets/header.html");
+  $("footer").load("/Website/assets/footer.html");
+});
--- assets/js/post.js
+++ assets/js/post.js
@@ -8,51 +8,43 @@ function getBlogPost() {
 }
 
 function loadPost(postId) {
-  document.addEventListener("DOMContentLoaded", function () {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/Website/blog/posts/" + postId + ".html", true);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(xhr.responseText, "text/html");
-        var postTitle = doc.querySelector(".post_title").textContent;
-        var postDate = doc.querySelector(".post_date").textContent;
-        var postContent = doc.querySelector(".post_content").textContent;
+  $(document).ready(function () {
+    $.get("Website/blog/posts/" + postId + ".html", function (data) {
+      var postTitle = $(data).find(".post_title").text();
+      var postDate = $(data).find(".post_date").text();
+      var postContent = $(data).find(".post_content").text();
 
-        document.querySelector("#blog-content").innerHTML =
-          '<h1 class="box_light">' +
+      $("#blog-content").html(
+        '<h1 class="box_light" >' +
           postTitle +
           "</h1>" +
           '<p class="small_body post_date">' +
           postDate +
           "</p>" +
-          '<article class="box"><p>' +
+          '<div class="box"><p>' +
           postContent +
-          "</p></article>";
-      }
-    };
-    xhr.send();
+          "</p></div>"
+      );
+    });
   });
 }
 
 async function loadPostList() {
   const postListHtml = [];
-  const data = await fetch("/Website/blog/posts/posts.json").then((res) =>
-    res.json()
-  );
+  const data = await $.ajax({
+    url: "/Website/blog/posts/posts.json",
+  });
 
-  for (let i = data.posts.length - 1; i >= 0; i--) {
+  for (let i = data.posts.length; i >= 0; i--) {
     const postId = data.posts[i].id;
     const postUrl = "/Website/blog/posts/" + postId + ".html";
-    const postData = await fetch(postUrl).then((res) => res.text());
+    const postData = await $.get(postUrl);
 
-    const parser = new DOMParser();
-    const postHtml = parser.parseFromString(postData, "text/html");
-
-    const postTitle = postHtml.querySelector(".post_title").textContent;
-    const postDate = postHtml.querySelector(".post_date").textContent;
-    const postContent = postHtml.querySelector(".post_content")
-      .textContent.trim()
+    const postTitle = $(postData).find(".post_title").text();
+    const postDate = $(postData).find(".post_date").text();
+    const postContent = $(postData)
+      .find(".post_content")
+      .text()
       .split(" ")
       .slice(0, 51)
       .join(" ");
@@ -60,18 +52,20 @@ async function loadPostList() {
     postListHtml.push(
       `<section>
         <div class="small_header">
-          <a href="/Website/blog/" class="blog-link" data-post-id="${postId}">
+          <a href="Website/blog/" class="blog-link" data-post-id="${postId}">
             <div class="prev_title">${postTitle}</div>
             <div class="prev_date">(${postDate})</div>
           </a>
         </div>
         <div class="small_body">
-          <p>${postContent}... <a href="/Website/blog/" class="blog-link" data-post-id="${postId}">read more</a></p>
+          <p>${postContent}... <a href="Website/blog/" class="blog-link" data-post-id="${postId}">  read more</a></p>  
         </div>
       </section>`
     );
-  }
-  document.querySelector("#blog-content").innerHTML = postListHtml.join("");
+  }  
+
+  $("#blog-content").html(postListHtml.join(""));
+  BlogClick();
 }
 
 function getUrlParameter(name) {
@@ -84,14 +78,11 @@ function getUrlParameter(name) {
 }
 
 function BlogClick() {
-  document.addEventListener('DOMContentLoaded', function () {
-    document.addEventListener('click', function(event) {
-      if (!event.target.classList.contains('blog-link')) {
-        return;
-      }
+  $(document).ready(function () {
+    $(document).on("click", ".blog-link", function (event) {
       event.preventDefault();
-      const postId = event.target.dataset.postId;
-      window.location.href = "/Website/blog/?postId=" + postId;
+      var postId = $(this).data("post-id");
+      window.location.href = "Website/blog/?postId=" + postId;
     });
   });
 }
