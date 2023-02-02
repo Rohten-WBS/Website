1acada4492fdfd72006d13707bdbc50cfc1a95b2
Rohten (WBS) <109280187+Rohten-WBS@users.noreply.github.com>
2023-02-02 08:16:10 +0100

    up

--- assets/js/assets_loader.js
+++ assets/js/assets_loader.js
@@ -1,24 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("/Website/assets/header.html")
-    .then((response) => response.text())
-    .then((header) => {
-      document.querySelector("header").innerHTML = header;
-    });
-  fetch("/Website/assets/footer.html")
-    .then((response) => response.text())
-    .then((footer) => {
-      document.querySelector("footer").innerHTML = footer;
-    });
-});
-
-function AboutUs() {
-  document.addEventListener("click", function (event) {
-    var dropdown = document.querySelector("#about-us-dropdown");
-    var aboutUs = document.querySelector("#about-us");
-    if (event.target === aboutUs) {
-      dropdown.style.display = dropdown.style.display === "none" ? "" : "none";
-    } else if (!event.target.closest("#about-us-dropdown")) {
-      dropdown.style.display = "none";
-    }
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/Website/assets/header.html", true);
+  xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+  document.querySelector("header").innerHTML = xhr.responseText;
+  }
+  };
+  xhr.send();
+  
+  var xhr2 = new XMLHttpRequest();
+  xhr2.open("GET", "/Website/assets/footer.html", true);
+  xhr2.onreadystatechange = function () {
+  if (xhr2.readyState === 4 && xhr2.status === 200) {
+  document.querySelector("footer").innerHTML = xhr2.responseText;
+  }
+  };
+  xhr2.send();
   });
-}
